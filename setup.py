29392b8f52c4d5d2ac36ceb81dfaebf048417e15
Arjeus Guevarra <killer2ma2@yahoo.com>
2023-08-26 17:35:05 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="RAFT-STEREO",
+    author="Lipson, Lahav and Teed, Zachary and Deng, Jia",
+    description="A short description of your package",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
